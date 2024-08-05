52344923dd137a6c9e1877fc5f1f1ef872d2e8e4
NiccoGrillo <niccolo.grillo@mail.polimi.it>
2024-08-05 12:24:07 +0200

    hgjdc

--- src/req1.py
+++ src/req1.py
@@ -2,10 +2,10 @@ import agents.MultiplicativePacingAgent as mpa
 import agents.UCB1BiddingAgent as ucb1
 import numpy as np
 
-if __name__ == 'main':
+if __name__ == '__main__':
     # Initialize the agents
     agent1 = mpa.MultiplicativePacingAgent(1, 1, 1, 1)
-    agent2 = ucb1.UCB1BiddingAgent(1, 1, 1, 1)
+    agent2 = ucb1.UCB1BiddingAgent(1, [.5, .5], 1, 1)
     # Run the agents
     # agent1.bid()
     # agent2.bid()
