9de7ac5ac3d998ab23a61c69e18c1d15cd5bc364
LucaBarda <53540072+LucaBarda@users.noreply.github.com>
2024-08-06 15:30:56 +0200

    set_seed

--- src/req1.py
+++ src/req1.py
@@ -1,10 +1,25 @@
 import agents as ag
 import auctions as au
 import environments as envi
+from utils import *
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+n_days = 500
+users_per_day = [10 for i in range(n_days)] #20 users per day
+ctrs = [0.8, 0.5, 0.9, 1] # company + competitors
+lambdas = [1, 0.9] # 2 slots
+budget = 1000
+product_cost = 0.1
+valuation = 1
+ucb_bidding_agent = True  #if true then the bidding agent is ucb-like, else it is multiplicative pacing
+
+n_trials = 10
+
+
+problem_params = {"n_days" : n_days, "users_per_day" : users_per_day, "ctrs" : ctrs, "lambdas" : lambdas,
+                  "ucb_bidding_agent" : ucb_bidding_agent, "budget" : budget, "product_cost" : product_cost, "valuation" : valuation}
 if __name__ == '__main__':
     # Initialize the agents
     agent1 = ag.MultiplicativePacingAgent(1, 1, 1, 1)
@@ -20,3 +35,4 @@ if __name__ == '__main__':
     auction1 = au.SecondPriceAuction([.5, .6])
 
     env1 = envi.StochasticBiddingCompetitors(1,1)
+    set_seed(1)
