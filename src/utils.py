a3834e522e08556e10aa7a98071e7aea0465c6ea
NiccoGrillo <niccolo.grillo@mail.polimi.it>
2024-08-06 17:30:04 +0200

    luha

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,13 @@ def set_seed(seed):
     np.random.seed(seed)
     return
 
+def generate_adv_conv_prob_sequence(len_seq):
+    return_array = np.zeros(len_seq)
+    alpha = 1
+    for i in range(len_seq):
+        beta = np.random.uniform(2, 8) #adversarial
+        conversion_prob = np.random.beta(alpha, beta)
+        # reward = conversion_prob * num_buyers * (price - cost_per_good)    
+        return_array[i] = conversion_prob
+    
+    return return_array
